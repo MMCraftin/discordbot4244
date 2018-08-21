b9f9c5b59b507738ec61a8691efa5d2385d7930a
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-21 12:20:51 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,25 +2,29 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  //!addrole @username rolename
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to do that.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user, yo.");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  let role = args.join(" ").slice(16);
+  if(!role) return message.reply("You need to specify the role you want to use.");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.reply("That Role Does Not Exist.");
 
   if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
   await(rMember.addRole(gRole.id));
 
   try{
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    await rMember.send(`You have been given the role ${gRole.name}`)
   }catch(e){
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    message.channel.send(`<@${rMember.id}> has been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
   }
 }
 
 module.exports.help = {
   name: "addrole"
 }
+
+//Made By MMCraftin
+//Discord Bot V0.2
