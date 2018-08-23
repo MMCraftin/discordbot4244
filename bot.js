aec02ea3b15232cb89573dfdfdb182766a894537
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-23 17:59:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const mysql = require("mysql")
+
+var con = mysql.createConnection({
+
+	host: "sql9.freemysqlhosting.net"
+	user: "sql9253430"
+	password: "7PT7quQX1R"
+	database: "sql9253430"
+
+});
+
+con.connect(err => {
+
+	if(err) throw err;
+	console.log("Connected To Database")
+
+});
 
 client.on('!ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+    if (message.content === '!serverset') {
     	message.reply('pong');
   	}
 });
