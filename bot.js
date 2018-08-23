2e7dfdea3eb07ea9962649748d87dbd4f64d90d5
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-23 17:28:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- client.on('ready', () => {
+
+client.on('!ready', () => {
     console.log('I am ready!');
 });
- client.on('message', message => {
-    if (message.content === '!peter') {
-    	message.reply('Is Gay');
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+    	message.reply('pong');
   	}
 });
- // THIS  MUST  BE  THIS  WAY
 
-
-client.on("guildMemberAdd", function(member) {
-    let role = member.guild.roles.find("name", "test");
-    member.addRole(role).catch(console.error);
-});
-
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
