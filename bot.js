eafed3733c375b830134d48818d0aaa4ad5553ea
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-23 18:47:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on('!ready', () => {
     console.log('I am ready!');
@@ -10,6 +11,8 @@ client.on('message', message => {
     	message.reply('We Play On Rusty|NY|3X|TP|Home|Insta|Clans|Kits|Loot|Events|Smelt The Ip Is 74.91.123.29:21215');
   	}
 });
+
+if(message.content.startsWith(prefix + "test"))
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
